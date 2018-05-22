24f268e2e27b3c24ae0b4ba05c13a17382d883e9
kf07 <furusu0715@gmail.com>
2018-05-22 14:50:39 +0900

    フォーム反映

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
 import React from 'react';
 
-const Child = (props) => (
-    <div onClick={props.handleClick}>
-        {props.data}
-    </div>
-);
 class App extends React.Component {
-    constructor(props) {
+    constructor(props){
         super(props);
-        this.state = {
-            value1: 'たける',
-            value2: [ 'bar', 'baz' ],
+        this.state= {
+            hp:'1200',
+            pp:'160'
         };
+        this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
+    }
+    handleChange(e) {
+
+        switch (e.target.name) {
+            case 'hp':
+                this.setState({
+                    hp: e.target.value,
+                });
+
+                break;
+            case 'pp':
+                this.setState({
+                    pp: e.target.value,
+                });
+                break;
+            default:
+        }
     }
 
     handleClick(){
-        this.setState(() => {
-            return { value1: 'たかし' };
-        });
+        const hp = this.state.hp;
+        const pp = this.state.pp;
+        console.log(hp,pp)
     }
 
-    render() {
-        return (
-            <div>
-                <Child data={this.state.value1} handleClick={this.handleClick} />
-            </div>
-        );
+    render(){
+        return(
+           <div className="form">
+               <input value={this.state.hp} onChange={this.handleChange} name="hp" />
+               <input value={this.state.pp} onChange={this.handleChange} name="pp" />
+               <button onClick={this.handleClick}>登録</button>
+               <p>HP:{this.state.hp}</p>
+               <p>PP:{this.state.pp}</p>
+           </div>
+        )
     }
 }
+
 export default App;
