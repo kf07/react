2d3559e522936b79eb57172beefd02fa68d4feb1
kf07 <furusu0715@gmail.com>
2018-05-23 17:06:39 +0900

    dispath

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const initialState = {
+    tasks:[]
+};
+
+function tasksReducer(state = initialState,action) {
+    switch(action.type){
+        case'ADD_TASK':
+            return{
+                ...state,
+                tasks:state.tasks.concat([action.task])
+            };
+        default:
+            return state;
+    }
+}
+
+const store = createStore(tasksReducer);
+
+const addTask = (task)=>({
+    type:'ADD_TASK',
+    payload:{
+        task
+    }
+});
+
+store.dispatch(addTask('Storeを学ぶ'));
+
+console.log(store.getState())
