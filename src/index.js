6ef4fd33a9ef2dda12a1b3d5beb14d62610058b6
kf07 <furusu0715@gmail.com>
2018-06-01 14:52:01 +0900

    計算するやつ

--- src/index.js
+++ src/index.js
@@ -1,8 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+let inputData = '',
+    inputNum,
+    results;
+
+const setTextData =(event) => {
+    inputData = event.target.value;
+    render();
+};
+
+const setNumData = (event) => {
+    inputNum = event.target.value;
+    render();
+};
+
+const MyInput =() => (
+    <div>
+        <input type="text" value={inputData} onChange={setTextData} />
+        <input type="text" value={inputNum} onChange={setNumData} />
+    </div>
+);
+
+const onPlus =() => {
+    results = Number(inputData) + Number(inputNum);
+    render();
+};
+
+const onMinus = () => {
+    results =  Number(inputData) - Number(inputNum);
+    render();
+};
+
+const Calc = () => (
+  <Result result = {results} />
+);
+
+const Result =({ result }) => (
+  <p>りざると：{result}</p>
+);
+
+const MyBtn =() => (
+    <div>
+        <button onClick={onPlus}>たす</button>
+        <button onClick={onMinus}>ひく</button>
+    </div>
+);
+
+const App =() =>(
+    <div>
+        <MyInput />
+        <MyBtn/>
+        <Calc />
+    </div>
+);
+
+
+
+const render = () => ReactDOM.render(<App />, document.getElementById('root'));
+render();
