3ab967c8912a49a2e62ae5ae13d30e5efe29062e
kf07 <furusu0715@gmail.com>
2018-06-19 17:21:42 +0900

    コメント追加

--- src/index.js
+++ src/index.js
@@ -4,14 +4,22 @@ import { createStore } from 'redux';
 import { Provider,connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+/* Storeの実装*/
+
+//初期state変数（initialState)の作成
 const initialState = {
     value : null,
 };
 
+//storeの作成　引数に関数として定義したReducerと初期State
 const store = createStore(formReducer,initialState);
 
+/* Actionの実装 */
+
+// Action名の定義
 const SEND = 'SEND';
 
+// Action Creator type属性SENDとViewから受け取ったvalueをもったactionを作成
 function send(value) {
     return {
         type: SEND,
@@ -19,7 +27,11 @@ function send(value) {
     };
 }
 
+/* Reducer */
 
+//現在の状態（state）と受け取ったActionを引数にとり新しい状態を返す関数
+
+//ActionのTYPEがSENDの場合は受け取ったActionのValue値でStoreのStateを更新して、それ以外のときはもともとのStateを返す
 function formReducer(state,action) {
     switch(action.type) {
         case SEND:
